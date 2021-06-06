7dbbc7422190e2459ad7e265e5428c348a0b98a2
Illya <illyasmirnickiy@gmail.com>
2021-06-05 23:17:20 -0700

    fourth

--- src/main/java/V1/Foo.java
+++ src/main/java/V1/Foo.java
@@ -1,38 +1,66 @@
 package V1;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Foo {
 
     private boolean second_bool = false;
     private boolean third_bool = false;
+    private Lock locker = new ReentrantLock();
+    private Condition condition = locker.newCondition();
 
-    public  void first() {
+    public void first() {
+
+        locker.lock();
 
         System.out.println("first");
 
         second_bool = true;
+
+        condition.signalAll();
+        locker.unlock();
+
     }
 
     public void second() {
-
-        while (!second_bool) {
-            Thread.yield();
+        locker.lock();
+        try {
+            while(!second_bool) {
+                condition.await();
         }
 
         System.out.println("second");
 
         second_bool = false;
         third_bool = true;
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            condition.signalAll();
+            locker.unlock();
+        }
     }
 
     public void third() {
+        locker.lock();
 
-        while(!third_bool) {
-            Thread.yield();
+        try {
+            while(!third_bool) {
+                condition.await();
         }
 
         System.out.println("third");
 
         third_bool = false;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            condition.signalAll();
+            locker.unlock();
+        }
     }
 }
 
